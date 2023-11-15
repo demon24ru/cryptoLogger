781760674ba0ca80592cc08ca10f18988af2a16d
demon-dem <ittek@inbox.ru>
2023-11-15 17:54:10 +0700

    upd

--- internal/storage/clickhouse.go
+++ internal/storage/clickhouse.go
@@ -85,7 +85,7 @@ func (c *ClickHouse) CommitTickers(appCtx context.Context, data []Ticker) error 
 
 	for i := range data {
 		ticker := data[i]
-		_, err := stmt.Exec(ticker.MktCommitName, ticker.Data, ticker.Timestamp.Format(clickHouseTimestamp))
+		_, err := stmt.Exec(ticker.ExchangeName, ticker.MktCommitName, ticker.Data, ticker.Timestamp.Format(clickHouseTimestamp))
 		if err != nil {
 			return err
 		}
@@ -110,7 +110,7 @@ func (c *ClickHouse) CommitTrades(appCtx context.Context, data []Trade) error {
 
 	for i := range data {
 		trade := data[i]
-		_, err := stmt.Exec(trade.MktCommitName, trade.Data, trade.Timestamp.Format(clickHouseTimestamp))
+		_, err := stmt.Exec(trade.ExchangeName, trade.MktCommitName, trade.Data, trade.Timestamp.Format(clickHouseTimestamp))
 		if err != nil {
 			return err
 		}
@@ -135,7 +135,7 @@ func (c *ClickHouse) CommitLevel2(appCtx context.Context, data []Level2) error {
 
 	for i := range data {
 		level2 := data[i]
-		_, err := stmt.Exec(level2.MktCommitName, level2.Data, level2.Timestamp.Format(clickHouseTimestampMicroSec))
+		_, err := stmt.Exec(level2.ExchangeName, level2.MktCommitName, level2.Data, level2.Timestamp.Format(clickHouseTimestampMicroSec))
 		if err != nil {
 			return err
 		}
@@ -160,7 +160,7 @@ func (c *ClickHouse) CommitOrdersBook(appCtx context.Context, data []OrdersBook)
 
 	for i := range data {
 		ordersBook := data[i]
-		_, err := stmt.Exec(ordersBook.MktCommitName, ordersBook.Sequence, ordersBook.Bids, ordersBook.Asks, ordersBook.Timestamp.Format(clickHouseTimestamp))
+		_, err := stmt.Exec(ordersBook.ExchangeName, ordersBook.MktCommitName, ordersBook.Sequence, ordersBook.Bids, ordersBook.Asks, ordersBook.Timestamp.Format(clickHouseTimestamp))
 		if err != nil {
 			return err
 		}
