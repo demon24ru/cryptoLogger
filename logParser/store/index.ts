ba322ce24246f3a0cedee96676b74d630de3fd90
demon-dem <ittek@inbox.ru>
2024-11-19 16:42:06 +0700

    upd

--- logParser/store/index.ts
+++ logParser/store/index.ts
@@ -33,103 +33,119 @@ export async function parseMarkets(): Promise<void> {
                     if (!markets.kucoin)
                         markets.kucoin = {}
                     for (const market of d.markets) {
-                        const resf = await fetch(`https://www.kucoin.com/_api/trade-front/trade-basic-info?symbol=${market.id}`, {method: 'GET'});
-                        if (!resf.ok) {
-                            continue
-                        }
-                        const result: any = await resf.json()
+                        try {
+                            const resf = await fetch(`https://www.kucoin.com/_api/trade-front/trade-basic-info?symbol=${market.id}`, {method: 'GET'});
+                            if (!resf.ok) {
+                                continue
+                            }
+                            const result: any = await resf.json()
 
-                        if (result.code === '100000') {
-                            continue
-                        }
-                        if (result.code !== '200') {
-                            logger.error(market.id, result)
-                            throw new Error(`Kucoin Spot Market ${market.id}, result.code not 200`);
-                        }
+                            if (result.code === '100000') {
+                                continue
+                            }
+                            if (result.code !== '200') {
+                                logger.error(market.id, result)
+                                throw new Error(`Kucoin Spot Market ${market.id}, result.code not 200`);
+                            }
 
-                        markets.kucoin[market.id.split('-').join('')] = {
-                            priceIncrement: Number(result.data.priceIncrement),
-                            multiplier: 1,
-                            config
+                            markets.kucoin[market.id.split('-').join('')] = {
+                                priceIncrement: Number(result.data.priceIncrement),
+                                multiplier: 1,
+                                config
+                            }
+                            await sleep(50);
+                            logger.log(`Load kucoin ${market.id}`)
+                        } catch (e) {
+                            logger.error(`Load kucoinFutures ${market.id}`, e.message, e.stack)
                         }
-                        await sleep(50);
-                        logger.log(`Load kucoin ${market.id}`)
                     }
                     break
                 case 'kucoinFutures':
                     if (!markets.kucoin)
                         markets.kucoin = {}
                     for (const market of d.markets) {
-                        const resf = await fetch(`https://www.kucoin.com/_api_kumex/web-front/contracts/${market.id}`, {method: 'GET'});
-                        if (!resf.ok) {
-                            continue
-                        }
-                        const result: any = await resf.json()
+                        try {
+                            const resf = await fetch(`https://www.kucoin.com/_api_kumex/web-front/contracts/${market.id}`, {method: 'GET'});
+                            if (!resf.ok) {
+                                continue
+                            }
+                            const result: any = await resf.json()
 
-                        if (result.code === '100000') {
-                            continue
-                        }
-                        if (result.code !== '200') {
-                            logger.error(market.id, result)
-                            throw new Error(`Kucoin Futures Market ${market.id}, result.code not 200`);
-                        }
+                            if (result.code === '100000') {
+                                continue
+                            }
+                            if (result.code !== '200') {
+                                logger.error(market.id, result)
+                                throw new Error(`Kucoin Futures Market ${market.id}, result.code not 200`);
+                            }
 
-                        markets.kucoin[`${market.id.slice(0, -1)}F`] = {
-                            priceIncrement: result.data.tickSize,
-                            multiplier: result.data.multiplier,
-                            config
+                            markets.kucoin[`${market.id.slice(0, -1)}F`] = {
+                                priceIncrement: result.data.tickSize,
+                                multiplier: result.data.multiplier,
+                                config
+                            }
+                            await sleep(50);
+                            logger.log(`Load kucoinFutures ${market.id}`)
+                        } catch (e) {
+                            logger.error(`Load kucoinFutures ${market.id}`, e.message, e.stack)
                         }
-                        await sleep(50);
-                        logger.log(`Load kucoinFutures ${market.id}`)
                     }
                     break
                 case 'bybit':
                     if (!markets.bybit)
                         markets.bybit = {}
                     for (const market of d.markets) {
-                        // tslint:disable-next-line:max-line-length
-                        const resf = await fetch(`https://api.bybit.com/v5/market/instruments-info?category=spot&symbol=${market.id}`, {method: 'GET'});
-                        if (!resf.ok) {
-                            continue
-                        }
-                        const result: any = await resf.json()
+                        try {
+                            // tslint:disable-next-line:max-line-length
+                            const resf = await fetch(`https://api.bybit.com/v5/market/instruments-info?category=spot&symbol=${market.id}`, {method: 'GET'});
+                            if (!resf.ok) {
+                                continue
+                            }
+                            const result: any = await resf.json()
 
-                        if (result.retCode !== 0) {
-                            logger.error(market.id, result)
-                            throw new Error(`Bybit Spot Market ${market.id}, result.code not 200`);
-                        }
+                            if (result.retCode !== 0) {
+                                logger.error(market.id, result)
+                                throw new Error(`Bybit Spot Market ${market.id}, result.code not 200`);
+                            }
 
-                        markets.bybit[market.id] = {
-                            priceIncrement: Number(result.result.list[0].priceFilter.tickSize),
-                            multiplier: 1,
-                            config
+                            markets.bybit[market.id] = {
+                                priceIncrement: Number(result.result.list[0].priceFilter.tickSize),
+                                multiplier: 1,
+                                config
+                            }
+                            await sleep(50);
+                            logger.log(`Load Bybit ${market.id}`)
+                        } catch (e) {
+                            logger.error(`Load Bybit ${market.id}`, e.message, e.stack)
                         }
-                        await sleep(50);
-                        logger.log(`Load kucoin ${market.id}`)
                     }
                     break
                 case 'bybitFutures':
                     if (!markets.bybit)
                         markets.bybit = {}
                     for (const market of d.markets) {
-                        const resf = await fetch(`https://api.bybit.com/v5/market/instruments-info?category=linear&symbol=${market.id}`, {method: 'GET'});
-                        if (!resf.ok) {
-                            continue
-                        }
-                        const result: any = await resf.json()
+                        try {
+                            const resf = await fetch(`https://api.bybit.com/v5/market/instruments-info?category=linear&symbol=${market.id}`, {method: 'GET'});
+                            if (!resf.ok) {
+                                continue
+                            }
+                            const result: any = await resf.json()
 
-                        if (result.retCode !== 0) {
-                            logger.error(market.id, result)
-                            throw new Error(`Bybit Futures Market ${market.id}, result.code not 200`);
-                        }
+                            if (result.retCode !== 0) {
+                                logger.error(market.id, result)
+                                throw new Error(`Bybit Futures Market ${market.id}, result.code not 200`);
+                            }
 
-                        markets.bybit[`${market.id}F`] = {
-                            priceIncrement: Number(result.result.list[0].priceFilter.tickSize),
-                            multiplier: 1,
-                            config
+                            markets.bybit[`${market.id}F`] = {
+                                priceIncrement: Number(result.result.list[0].priceFilter.tickSize),
+                                multiplier: 1,
+                                config
+                            }
+                            await sleep(50);
+                            logger.log(`Load BybitFutures ${market.id}`)
+                        } catch (e) {
+                            logger.error(`Load BybitFutures ${market.id}`, e.message, e.stack)
                         }
-                        await sleep(50);
-                        logger.log(`Load kucoinFutures ${market.id}`)
                     }
                     break
             }
